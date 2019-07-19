c6bed110dca618d60d0e66e1734da046d0290cde
chenjing.serena <chenjing.serena@bytedance.com>
2019-07-19 10:36:35 +0800

    feat: init repo

---
+++ tests/lib/rules/blacklist.js
@@ -0,0 +1,62 @@
+// const rule = require('../../../lib/rules/blacklist');
+// const RuleTester = require('eslint').RuleTester;
+
+// const ruleTester = new RuleTester({
+//   parserOptions: {
+//     ecmaVersion: 6,
+//     sourceType: 'module'
+//   }
+// })
+
+// ruleTester.run('string-value-blacklist', rule, {
+//   valid: [
+//     {
+//       code: ' var a = "abc";',
+//       options: [{
+//         regexpPatterns: [/d/, /e/]
+//       }]
+//     },
+//     {
+//       code: ' var a = "abc"; ',
+//       options: [{
+//         regexpPatterns: [/a/],
+//         ignoreValues: ['abc']
+//       }]
+//     }
+//   ],
+
+//   invalid: [
+//     {
+//       code: ' var a = "abc"; ',
+//       options: [{
+//         regexpPatterns: [/a/]
+//       }],
+//       errors: [{
+//         message: 'error',
+//         rule: 'string-value-blacklist'
+//       }]
+//     },
+//     {
+//       code: ' var arr = ["abc"]; ',
+//       options: [{
+//         regexpPatterns: [ /a/]
+//       }],
+//       errors: [{
+//         message: 'error',
+//         rule: 'string-value-blacklist'
+//       }]
+//     },
+//     {
+//       code: ' var obj = { str: "abc" };',
+//       options: [{
+//         regexpPatterns: [/a/],
+//       }]
+//     },
+//     {
+//       code: ' function () { return "abc" }',
+//       options: [{
+//         regexpPatterns: [/a/],
+//       }]
+//     },
+//   ]
+// })
